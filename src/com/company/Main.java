86261c25ba15087be79837114fd686bdd6d3c549
Diego Santiago <diegosantiago@knights.ucf.edu>
2021-09-13 03:48:20 -0400

    Exercise 1

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Diego Santiago
+ */
+
+
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner gorgino = new Scanner(System.in);
+        System.out.print("What is your name?");
+
+        String bruh = gorgino.nextLine();
+        System.out.println("Hello, " + bruh);
+    }
+}
